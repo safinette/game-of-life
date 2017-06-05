f4271d5c8d2b4e4b0de17bc44d77f9aad989b38c
safinette <safinette_74@email.com>
2017-06-05 14:46:44 +0200

    build ok

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -9,7 +9,8 @@ package com.wakaleo.gameoflife.domain;
  */
 public enum Cell {
     //SG test build auto
-    LIVE_CELL("*"), DEAD_CELL(".");
+    //SG tests ko * en - 
+    LIVE_CELL("-"), DEAD_CELL(".");
 
     private String symbol;//SG commenter pour pas que ca compile
 
